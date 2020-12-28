b26136926044f433149f0679bdc4e7e09c643c89
Ganna3 <49812586+Ganna3@users.noreply.github.com>
2020-12-29 00:57:01 +0200

    Update optimal.java

--- optimal.java
+++ optimal.java
@@ -1,35 +1,76 @@
-package projectos2;
 
-  
+
+
+    
+
+
+package project.os2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 
-public class optimal {
-    
-     private int pf = 0;
-       private  int n;
-       private int pages[];
-       private int NumOfFrames=0;
-       private boolean HIT[];
-    
-       private int outputView[][];
-       private LinkedList<Integer> indexes;
+public class optimal  {
+    private int pf = 0;
+    private  int n;
+    private int pages[];
+    private int NumOfFrames=0;
+    private boolean HIT[];
+    private int emp[];
+    private int outputView[][];
+    private LinkedList<Integer> indexes;
+    private String File;
+    public void User()throws IOException
+    { 
+        Scanner in1=new Scanner(System.in);
+        System.out.print("Enter the number of Frames \n");
+        NumOfFrames=in1.nextInt();
+        System.out.print("Enter the number of Page refrences you are going to enter \n");
+        n=in1.nextInt();
+        System.out.print("Enter the page refrences :\n");
+        pages=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            System.out.print("Page["+i+"]: ");
+            pages[i]=in1.nextInt();
+        }
+    }
+    public void FileUser() throws FileNotFoundException, IOException
+        {
+           System.out.print("Enter the name of the file: \n");
+           Scanner in=new Scanner(System.in);
+           File=in.nextLine();
+           File F = new File(File);
+          if(F.exists())
+          {
+            BufferedReader R = new BufferedReader(new FileReader(File));
+            NumOfFrames = Integer.parseInt(R.readLine());
+            System.out.print("Number of Frames: "+NumOfFrames+"\n");
+            n=Integer.parseInt(R.readLine());
+            System.out.print("Number of Page refrences: "+n+"\n");
+            String Line = R.readLine();
+            System.out.print(Line+"\n");
+            pages=new int[n];
+            Line = R.readLine();
+            StringTokenizer t= new StringTokenizer(Line,",");
+            for (int i = 0; i < n; i++)
+            {
+             pages[i]=Integer.parseInt(t.nextToken());
+             System.out.print(pages[i]+" ");
+            }
+            System.out.print("\n");
+          }
+        }
        void OPT() 
         {  
             System.out.println("====OPTIMAL====");
-        
-         Scanner in=new Scanner(System.in);
-        
-             System.out.println("How many frames do you want:");
-             NumOfFrames=in.nextInt();
-             
-        
-             System.out.println("How many pages are you going to enter:");
-             n=in.nextInt();
-             
              indexes=new LinkedList<>();
-             pages=new int[n];
               HIT = new boolean[n];
          outputView = new int[n][NumOfFrames];
               for (int i=0; i<n; i++) 
@@ -39,15 +80,9 @@ public class optimal {
                          outputView[i][j] =-1;
                      }
                 }
-             System.out.println("Enter your pages sequentially");
-              for (int i=0; i<n; i++) 
-                { 
-                   pages[i]=in.nextInt();
-                }
-          
            for (int i=0; i<n; i++) 
         { 
-           //System.out.print("Add Page : "+pages[i]+"\n");
+         
             if (indexes.size() < NumOfFrames) 
             { 
                 if(indexes.contains(pages[i]))
@@ -68,33 +103,75 @@ public class optimal {
                 }
                  else
                 {
-                    int max=-1;
-                    //System.out.print("(Miss) \n");
+                    int max=-2;
+                    int index=-1;
+                    
                     int ex[]=new int[NumOfFrames];
                     int ex2[]=new int[NumOfFrames];
+                     boolean flag[] = new boolean[NumOfFrames];
+                     for(int f=0;f<NumOfFrames;f++)
+                            {
+                              ex[f]=-1;  
+                              
+                            
+                            
+                            }
+                    
                         for(int z=i+1;z<n;z++)
                         {
                             for(int f=0;f<NumOfFrames;f++)
                             {
                                 ex2[f]=indexes.get(f);
-                                if(ex2[f]==pages[z])
+                                if(ex2[f]==pages[z] && flag[f]==false)
                                 {
                                     ex[f]=z;
+                                    flag[f]=true;
                                  
                                 }
+                            
                             }
                         }
                      
+                      
+                       boolean ok=true;
+                        
+                                for (int d=0;d<NumOfFrames;d++)
+                                {
+                                    
+                                      if(ex[d]==-1)
+                                         {
+                                              index=d;
+                                         
+                                         ok=false;
+                                            }
+                                              
+                                                
+                                }
+                                if (ok==false){
+                                
+                                      indexes.set(index,pages[i]);
+                                         pf++; 
+                                }
+                           
+                                
+                                
+                                if (ok==true){
                          for(int x=0;x<NumOfFrames;x++)
                             {
                                 if(ex[x]>max)
                                 {
                                     max=ex[x];
                                 }
+                               
+                                    
                             } 
                          int f=indexes.indexOf(pages[max]);
                          indexes.set(f,pages[i]);
-                  pf++;   
+                  pf++; 
+                                }
+                  
+                  
+                  
                  } 
           //  System.out.print("\n");
             }
@@ -136,189 +213,3 @@ public class optimal {
         System.out.println("Total number of Page faults: " + pf);
     } 
 }
-        
-  /*  private  int noOfFrames = 0;
-    private  int pageReferenceLenght;
-    private int numberOfPageFaults = 0;
-    private int PT1 = 0;
-    private  boolean isFULL = false;
-        
-    private int FRAMS[];
-    private int pageRefrenceString[];
-    private boolean HIT[];
-    private int outputView[][];
-       
-       
-       
-    public void OPT() {
-       System.out.println("====OPTIMAL====");
-       
-    Scanner in = new Scanner(System.in);
-
-    System.out.println("How many frames do you want:");
-    noOfFrames = in.nextInt();
-
-    System.out.println("How many pages are you going to enter:");
-    pageReferenceLenght = in.nextInt();
-    
-    FRAMS = new int[noOfFrames];
-    pageRefrenceString= new int[pageReferenceLenght];
-    HIT = new boolean[pageReferenceLenght];
-    outputView = new int[pageReferenceLenght][noOfFrames];
-   
-      
-        for(int u = 0; u < noOfFrames; u++)
-        {
-            FRAMS[u] = -1;
-        }
-
-        System.out.println("Enter your pages sequentially:");
-        for(int u = 0; u < pageReferenceLenght; u++)
-        {
-            pageRefrenceString[u] =in.nextInt();
-        }
-        System.out.println();
-        
-        
-        
-        for(int u = 0; u < pageReferenceLenght; u++)
-        {
-            int SEARCH = -1;
-            
-            for(int v = 0; v < noOfFrames; v++)
-            {
-                if(FRAMS[v] == pageRefrenceString[u])
-                {
-                    SEARCH = v;
-                    HIT[u] = true;
-                  
-                    break;
-                }
-            }
-
-            if(SEARCH == -1)
-            {
-                if(isFULL)
-                {
-                 
-                    int pageIndex=-1;
-                    boolean flag[];
-                    flag=new boolean[noOfFrames];
-                    for(int v = u + 1; v < pageReferenceLenght; v++)
-                    {
-                       
-                        for(int s = 0; s < noOfFrames; s++)
-                        {
-                            if((pageRefrenceString[v] == FRAMS[s]) ) 
-                                   
-                            {
-                                 pageIndex=s;
-                                 flag[s]=true;
-                               
-                               break;
-                            }
-                          
-                            
-                        }
-                    }
-                   
-                     PT1= 0;
-                
-                     int countF=0;
-                     for(int i=0;i<noOfFrames;i++){
-                         if (flag[i]==true)
-                         {
-                         countF++;
-                         }
-                     }
-                     
-                     // for(int i=0;i<noOfFrames;i++){
-                      if(countF==noOfFrames){
-                       FRAMS[pageIndex] = pageRefrenceString[u];
-                numberOfPageFaults++; 
-                      }
-                      
-                      //}
-                       if (countF >0 && countF<noOfFrames){
-                             FRAMS[pageIndex+1] = pageRefrenceString[u];
-                numberOfPageFaults++; 
-                       }
-                      if (countF==0){
-                             FRAMS[0] = pageRefrenceString[u];
-                numberOfPageFaults++;
-                      
-                      }
-                
-                
-                }
-                
-                           if(!isFULL)
-                               {
-                                   for (int i=0;i<noOfFrames;i++){
-                                         if (FRAMS[i]== -1){
-                                             PT1=i;
-                                             break;
-                                           }
-                                   }
-                                         
-                                 FRAMS[PT1] = pageRefrenceString[u];
-                numberOfPageFaults++;
-                PT1++;
-                                  if(PT1 == noOfFrames)
-                                     {
-                                     PT1= 0;
-                                     isFULL = true;
-                                     }
-                                }    
-                
-                         }
-     
-               
-               for(int v= 0; v < noOfFrames; v++)
-            {
-                outputView[u][v] = FRAMS[v];
-            }
-               
-               
-            }
-
-           
-        
-        
-        // *view data* //
-
-        for(int u = 0; u < pageReferenceLenght; u++)
-        {
-            System.out.print(pageRefrenceString[u] + ": frames content: ");
-            for(int v = 0; v < noOfFrames; v++)
-            {
-                if (outputView[u][v] == -1)
-                {
-                    System.out.print("   ");
-                    System.out.print("-");
-                }
-                else
-                {
-                     System.out.print("   ");
-                    System.out.print(outputView[u][v]);
-                    
-                   
-                }
-            }
-            System.out.print(": ");
-            if (HIT[u]) {
-                System.out.print(" #/HIT/#");
-            } else
-            {
-                System.out.print(" #/MISS/#");
-            }
-         
-            System.out.println();
-        }
-        System.out.println("Total number of Page faults: " + numberOfPageFaults);
-    }
-       }*/
-
-
-    
-
