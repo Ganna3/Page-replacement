6f61f0eee3588db619c60ac04b7ddec081d8ba51
Ganna3 <49812586+Ganna3@users.noreply.github.com>
2020-12-29 00:59:11 +0200

    Add files via upload

--- FIFO.java
+++ FIFO.java
@@ -1,59 +1,83 @@
+package project.os2;
 
-package projectos2;
-
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.LinkedList; 
 import java.util.Queue;
 import java.util.Scanner;
-
+import java.util.StringTokenizer;
 
 
 public class FIFO {
     private Queue<Integer> indexes= new LinkedList<>();
-        private LinkedList<Integer> replace ;
-       
-        
-         private int pf = 0;
-       private  int n;
-       private int pages[];
-        private int NumOfFrames=0;
-         private boolean HIT[];
-          private int outputView[][];
-         
-     void fifo() 
+    private LinkedList<Integer> replace ;
+    private boolean emp[];
+    private int pf=0;
+    private  int n;
+    private int pages[];
+    private int NumOfFrames;
+    private boolean HIT[];
+    private int outputView[][];
+    private String File;
+    public void User()throws IOException
+    { 
+        Scanner in1=new Scanner(System.in);
+        System.out.print("Enter the number of Frames \n");
+        NumOfFrames=in1.nextInt();
+        System.out.print("Enter the number of Page refrences you are going to enter \n");
+        n=in1.nextInt();
+        System.out.print("Enter the page refrences :\n");
+        pages=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            System.out.print("Page["+i+"]: ");
+            pages[i]=in1.nextInt();
+        }
+    }
+    public void FileUser() throws FileNotFoundException, IOException
+        {
+           System.out.print("Enter the name of the file: \n");
+           Scanner in=new Scanner(System.in);
+           File=in.nextLine();
+           File F = new File(File);
+          if(F.exists())
+          {
+            BufferedReader R = new BufferedReader(new FileReader(File));
+            NumOfFrames = Integer.parseInt(R.readLine());
+            System.out.print("Number of Frames: "+NumOfFrames+"\n");
+            n=Integer.parseInt(R.readLine());
+            System.out.print("Number of Page refrences: "+n+"\n");
+            String Line = R.readLine();
+            System.out.print(Line+"\n");
+            pages=new int[n];
+            Line = R.readLine();
+            StringTokenizer t= new StringTokenizer(Line,",");
+            for (int i = 0; i < n; i++)
+            {
+             pages[i]=Integer.parseInt(t.nextToken());
+             System.out.print(pages[i]+" ");
+            }
+            System.out.print("\n");
+          }
+        }
+     void FIFO() 
     {    
-        System.out.println("====FIFO====");
+        System.out.println("----FIFO----");
         
-         Scanner in=new Scanner(System.in);
-        
-             System.out.println("How many frames do you want:");
-             NumOfFrames=in.nextInt();
-             
-        
-             System.out.println("How many pages are you going to enter:");
-             n=in.nextInt();
-             
-             replace=new LinkedList<>();
-             pages=new int[n];
-              HIT = new boolean[n];
-         outputView = new int[n][NumOfFrames];
-            
-             for (int i=0; i<n; i++) 
-                { 
-                    for (int j=0; j<NumOfFrames; j++) 
-                     {
-                         outputView[i][j] =-1;
-                     }
-                }
-         
-             
-             System.out.println("Enter your pages sequentially");
-              for (int i=0; i<n; i++) 
-                { 
-                   pages[i]=in.nextInt();
-                }
-              
-             
+        Scanner in=new Scanner(System.in);
+        replace=new LinkedList<>();
+        HIT = new boolean[n];
+        outputView = new int[n][NumOfFrames];
+        for (int i=0; i<n; i++) 
+        { 
+         for (int j=0; j<NumOfFrames; j++) 
+          {
+            outputView[i][j] =-1;
+          }
+        }
         for (int i=0; i<n; i++) 
         { 
            
@@ -62,16 +86,13 @@ public class FIFO {
                 if(indexes.contains(pages[i]))
                 {
                     HIT[i]=true;
-                
                 }
                 else 
                 {  
-                    
                     replace.add(pages[i]);
                     pf++;
                     indexes.add(pages[i]); 
-                } 
-             
+                }
             } 
             else
             { 
@@ -94,17 +115,16 @@ public class FIFO {
              for(int v= 0; v < replace.size(); v++)
             {
                 outputView[i][v] = replace.get(v);
-            }
-                    
+            }    
         } 
       
        
         
-        // *view data* //
+        // view data //
 
         for(int u = 0; u < n; u++)
         {
-            System.out.print(pages[u] + ": frames content: ");
+            System.out.print(pages[u] + ": Frames content: ");
             for(int v = 0; v < NumOfFrames; v++)
             {
                 if (outputView[u][v] == -1)
@@ -136,4 +156,3 @@ public class FIFO {
      
       
 }
-
